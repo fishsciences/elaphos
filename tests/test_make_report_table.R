c221b0e40146ff8c38c50678bd13aef6f1a00130
Myfanwy <myfanwyjohnston@CFS-0298.local>
2022-03-22 13:37:54 -0700

    add small tests for make_detection_table

--- tests/test_make_report_table.R
+++ tests/test_make_report_table.R
@@ -1,5 +1,5 @@
 library(elaphos)
-qpcr_file = system.file("test_qpcr_data/test_qpcr_data/2022-01-18_Mont_Bhoo_P1.xls", package = "elaphos")
+qpcr_file = system.file("test_qpcr_data/2022-01-18_R2_BHOO_P_2.xls", package = "elaphos")
 
 if(file.exists(qpcr_file)){
     d2 = read_qpcr(qpcr_file, Experiment = "test")
@@ -10,4 +10,9 @@ if(file.exists(qpcr_file)){
     ans = elaphos:::make_detection_table(d2)
     
     stopifnot(all(colnames(ans) %in% c("FilterID", "BHOO", "test")))
+    # make sure no filterids have been added
+    stopifnot(all(ans$FilterID %in% d2$FilterID))
+    # make sure no filterIDs have been dropped
+    stopifnot(all(d2$FilterID %in% ans$FilterID))
 }
+
